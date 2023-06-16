f44de44ba3cb86d2af6371ef8d1f424e734cb278
pabloTokuo <pbo_1996@hotmail.com>
2023-06-16 17:08:27 -0300

    Weather App finalizado

---
+++ src/App.jsx
@@ -0,0 +1,141 @@
+import './app.css';
+import { useState } from 'react';
+import AirIcon from '@mui/icons-material/Air';
+import ThermostatIcon from '@mui/icons-material/Thermostat';
+import OpacityIcon from '@mui/icons-material/Opacity';
+
+const api = {
+  key: '08775b6ce804a7cb6c1c56af309ea69a',
+  base: 'https://api.openweathermap.org/data/2.5/',
+};
+
+function App() {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  async function search(e) {
+    if (e.key === 'Enter') {
+      const response = await fetch(
+        `${api.base}weather?q=${query}&units=metric&APPID=${api.key}`,
+      );
+      const data = await response.json();
+      setWeather(data);
+      setQuery('');
+      console.log(data);
+    }
+  }
+
+  const dates = new Date();
+  let month = months[dates.getMonth()];
+  let day = days[dates.getDay()];
+
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  return (
+    <div className="container">
+      <div
+        className={
+          typeof weather.main != 'undefined'
+            ? weather.main.temp > 16
+              ? 'weather-box weather-cloud'
+              : 'weather-box weather-rainy'
+            : 'weather-box'
+        }
+      >
+        <div className="weather-day">
+          <p>
+            <b>
+              {dates.getDate()} {month},
+            </b>{' '}
+            {day}
+          </p>
+        </div>
+        <input
+          type="text"
+          className="weather-input"
+          placeholder="Search City..."
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          onKeyDown={search}
+        />
+        {typeof weather.main != 'undefined' ? (
+          <>
+            <h1>{Math.round(weather.main.temp)}ºC</h1>
+            <h2>
+              {weather.name}, {weather.sys.country}
+            </h2>
+          </>
+        ) : (
+          ''
+        )}
+        <p className="weather-message">{weather.message}</p>
+      </div>
+      <div className="weather-infos">
+        <div className="weather-cards">
+          <ul>
+            <li>
+              <AirIcon />
+            </li>
+            <li>Wind</li>
+            <li>
+              {typeof weather.wind != 'undefined'
+                ? `${Math.round(weather.wind.speed)} km/h`
+                : ''}
+            </li>
+          </ul>
+        </div>
+        <div className="weather-cards">
+          <ul>
+            <li>
+              <ThermostatIcon />
+            </li>
+            <li>Pressure</li>
+            <li>
+              {typeof weather.main != 'undefined'
+                ? `${weather.main.pressure} MB`
+                : ''}
+            </li>
+          </ul>
+        </div>
+        <div className="weather-cards">
+          <ul>
+            <li>
+              <OpacityIcon />
+            </li>
+            <li>Humidity</li>
+            <li>
+              {typeof weather.main != 'undefined'
+                ? `${weather.main.humidity}%`
+                : ''}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
